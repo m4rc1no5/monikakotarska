5de5782c4925fd2c86f7efcc36dcf86d7801a127
Marcin Zaremba <marcin.zaremba@gmail.com>
2015-10-27 21:31:39 +0100

    Install symfony twig-bridge

--- web/index.php
+++ web/index.php
@@ -4,6 +4,10 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
+
 $app->get('/hello/{name}', function ($name) use ($app) {
     return 'Hello '.$app->escape($name);
 });
