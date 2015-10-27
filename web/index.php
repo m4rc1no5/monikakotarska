500a927e56e6de42f936c5de5b74785c364efda9
Marcin Zaremba <marcin.zaremba@gmail.com>
2015-10-27 21:40:21 +0100

    Test twig

--- web/index.php
+++ web/index.php
@@ -3,13 +3,16 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
+$app['debug'] = true;
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
+    'twig.path' => __DIR__.'/../views',
 ));
 
 $app->get('/hello/{name}', function ($name) use ($app) {
-    return 'Hello '.$app->escape($name);
+    return $app['twig']->render('hello.twig', array(
+        'name' => $name,
+    ));
 });
 
 $app->run();
